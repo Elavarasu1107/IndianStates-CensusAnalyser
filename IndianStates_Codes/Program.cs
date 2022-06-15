e43d7381e0bedfb71e499fa1c6da912f026296c1
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-15 17:56:16 +0530

    [Elavarasu] UC1-Loading Census Data from CSV File

---
+++ IndianStates_Codes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IndianStates_Codes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filePath = @"D:\BridgeLabz\IndianStates-CensusAnalyser\IndianStates_Codes\StateCensusData.csv";
+            StatesAnalyser getMethod = new StatesAnalyser();
+            getMethod.DataAnalyser(filePath);
+        }
+    }
+}
+
